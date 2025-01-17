843da572b091acf92e0186430ee4cb5ecf4af268
Pavlo Balandin <pashabalandin1200@gmail.com>
2025-01-17 11:35:32 +0200

    Initial commit

---
+++ jsonserver/provider.go
@@ -0,0 +1,16 @@
+package jsonserver
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{
+			"jsonserver_user": resourceUser(),
+		},
+		DataSourcesMap: map[string]*schema.Resource{
+			"jsonserver_user": dataSourceUser(),
+		},
+	}
+}
